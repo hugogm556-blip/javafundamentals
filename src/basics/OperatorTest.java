fbc5b0ef9c348660168a80fc0dc511c669304a05
hugogm556-blip <hugogm556@gmail.com>
2025-09-26 13:03:41 +0200

    caka

---
+++ src/basics/OperatorTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package basics;
+
+/**
+ * @author hugog26 sept 2025
+ */
+public class OperatorTest {
+	public static void main(String[] args) {
+		aritmeticOperations();
+		// declaring three variables of the same type
+		aritmeticOperations();
+		aritmeticOperations();
+	}
+
+	/**
+	 * 
+	 */
+	private static void aritmeticOperations() {
+		// TODO Auto-generated method stub
+		float num1=10, num2=12.341f, num3; 
+		System.out.println(num1);
+		//
+		num1= 23.34f;
+		System.out.println(num1);
+		// addition
+		float result = num1 + num2;
+		System.out.println("the result is " + result);
+		/*
+		 *  operacion de resta 
+		 *  1 el cpu lee el valor de la variable result 
+		 *  2 se realiza la resta  con el numero 1 
+		 *  3 se guarda el resultado en la misma  varible result
+		 *  
+		 *  el orden de la operacion es de la derecha a la izquierda 
+		 */
+		// es lo mismo que el de abajo  -=
+		result = result -1;
+		System.out.println("the result is " + result);
+
+		result-=1;
+		System.out.println("the result is " + result);
+		//multiplicacion
+		result=result*2;
+		
+		System.out.println("the result is " + result);
+		result*=2;
+		System.out.println("the result is " + result);
+		// division 
+		result= result /2;
+		System.out.println("the result is " + result);
+		result/=2;
+		System.out.println("the result is " + result);
+	}
+
+}
--- src/datastructure/ArrayTest.java
+++ src/datastructure/ArrayTest.java
@@ -1,23 +1,18 @@
 package datastructure;
 
+import java.util.Iterator;
+
 public class ArrayTest {
 	public static void main(String[] args) {
 		// ==args igualar un valor
 		// establecer un valor a una variable
 		System.out.println(1 == 1);
-		int total = 0;
+
 		int[] numbers = { 12, 123123, 435 };
-		// sirve para sumar las posiciones 
-		// int sum= numbers [0]+  numbers [1];
-		// esta script sirve para sumar los numeros de una lista primero hacemos el bucle para que recorra la lista y lo registre en la variable 
-		// total luego en la variable total y se va sumando en total primero la primera variable y luego la segunda variable y asi hasta la tercera
-		for (int i = 0; i < numbers.length; i++) {
-			System.out.println("t = " + total);
-			total =numbers[i] + total ;
-			// numbers[i] sirve para recoger  
-		}
-		System.out.println(total);
+
 		// array
+		sumup(numbers);
+		findMaxMix(numbers);
 		// crea dos objetos de usuario
 		User[] clients = new User[10];
 		// tiene capacidad de guardar 20000 usuarios
@@ -25,7 +20,7 @@ public class ArrayTest {
 		clients[1] = new User(24, "aday", true, "lolo", 1.7f, "male", 20);
 
 		System.out.println(numbers.length);
-	
+
 		// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 2
 		// out of bounds for length 2
 		// at
@@ -51,5 +46,56 @@ public class ArrayTest {
 		System.out.println(users.length);
 		System.out.println(users[2]);
 
+	}
+
+	/**
+	 * find the maximun and minimun value in the array
+	 * @param numbers
+	 */
+	private static void findMaxMix(int[] numbers) {
+		// TODO Auto-generated method stub
+		int max= numbers [0];
+		int min= numbers [numbers.length -1]; 
+	
+		for (int i = 0; i < numbers.length ; i++) {
+			// si max es mayor que dato actual que hemos cogido 
+			// numero[i]
+			/*
+			 * si max es mmenor que el numbers[i] ser cambia el numero mayor 
+			 * de numbers a el maximo numero 
+			 */
+			if (min > numbers[i]) {
+				min = numbers[i];
+				System.out.println("el min es " + min); 
+			}
+			if (max < numbers[i]) {
+				max = numbers[i];
+				System.out.println("el max es " + max);
+				
+		
+			}
+		}
+	
+	}
+
+	/**
+	 * @param numbers
+	 * 
+	 */
+	private static void sumup(int[] numbers) {
+
+		int total = 0;
+		// sirve para sumar las posiciones
+		int sum = numbers[0] + numbers[1];
+		// esta script sirve para sumar los numeros de una lista primero hacemos el
+		// bucle para que recorra la lista y lo registre en la variable
+		// total luego en la variable total y se va sumando en total primero la primera
+		// variable y luego la segunda variable y asi hasta la tercera
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println("t = " + total);
+			total = numbers[i] + total;
+			// numbers[i] sirve para recoger
+		}
+		System.out.println(total);
 	}
 }
